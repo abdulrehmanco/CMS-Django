98af4015ce359019e7343f9412de3e120e46a8eb
name <abdulrehmanrais123gmail.com>
2024-11-04 12:21:03 +0500

    SOme changes for static file

--- dms/urls.py
+++ dms/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
